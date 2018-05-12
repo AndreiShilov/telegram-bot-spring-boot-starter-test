47248695d8dbf534c91fd4d0229bf9ba2a73918a
zhaoyi <zhaoyi@kuainiugroup.com>
2018-05-12 10:04:41 +0800

    Make telgrambots starter work

--- src/main/java/com/github/andreishilov/bot/starter/TelegramBotSpringBootStarterDemoApplication.java
+++ src/main/java/com/github/andreishilov/bot/starter/TelegramBotSpringBootStarterDemoApplication.java
@@ -3,10 +3,8 @@ package com.github.andreishilov.bot.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.starter.EnableTelegramBots;
 
 @SpringBootApplication
-@EnableTelegramBots
 public class TelegramBotSpringBootStarterDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/github/andreishilov/bot/starter/TestBotConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.andreishilov.bot.starter;
+
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.starter.EnableTelegramBots;
+
+@Configuration
+@EnableTelegramBots
+public class TestBotConfiguration {
+}
